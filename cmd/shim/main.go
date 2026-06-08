12717921d2751fe9b0eee50894e3299a4ad39a32
Nicco <nicco@example.com>
2026-06-08 21:16:51 +0200

    feat: xdg-open shim

--- cmd/shim/main.go
+++ cmd/shim/main.go
@@ -1,1 +1,35 @@
-package main; func main() {}
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/nicco/remote-open/internal/config"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+	url := os.Args[1]
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+		return
+	}
+
+	cfg, err := config.Load(config.DefaultPath())
+	if err != nil {
+		log.Printf("config error: %v", err)
+		return
+	}
+
+	resp, err := http.Post(cfg.Server+"/open", "text/plain", strings.NewReader(url))
+	if err != nil {
+		log.Printf("post error: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	io.Copy(io.Discard, resp.Body)
+}
