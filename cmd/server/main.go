c99fab1f0505d95e000f497192ae3b0c33f0b6b6
Nicco <nicco@example.com>
2026-06-08 21:14:50 +0200

    feat: server entry point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,25 @@
-package main; func main() {}
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/nicco/remote-open/internal/server"
+)
+
+func main() {
+	port := flag.Int("port", 20080, "port to listen on")
+	flag.Parse()
+
+	hub := server.NewHub()
+	go hub.Run()
+
+	handler := server.NewHandler(hub)
+	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("remote-open-server listening on %s", addr)
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+}
