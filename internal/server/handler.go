cdb869fee3317b3a810a35e1da18b1aff5780cab
Nicco <nicco@example.com>
2026-06-08 21:13:53 +0200

    feat: server HTTP/WS handlers

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,1 +1,54 @@
 package server
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/nicco/remote-open/internal/protocol"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+type Handler struct{ hub *Hub }
+
+func NewHandler(hub *Hub) *Handler { return &Handler{hub: hub} }
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/open":
+		h.handleOpen(w, r)
+	case "/ws":
+		h.handleWS(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func (h *Handler) handleOpen(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	body, _ := io.ReadAll(r.Body)
+	url := string(body)
+	log.Printf("open-url: %s", url)
+	msg := protocol.OpenURL{Type: protocol.TypeOpenURL, URL: url}
+	data, _ := json.Marshal(msg)
+	h.hub.Broadcast <- data
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) handleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("ws upgrade error: %v", err)
+		return
+	}
+	log.Printf("client connected")
+	h.hub.Register <- conn
+}
